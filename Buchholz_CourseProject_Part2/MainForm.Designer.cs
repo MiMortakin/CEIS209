c369c7ad6a7bfb2678ed17bfaffc9d26fc9d5a55
MiMortakin <sea.chellers@gmail.com>
2024-11-17 23:20:37 -0800

    Week 4: Project Part 2 Started - Employee Payroll System

---
+++ Buchholz_CourseProject_Part2/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Buchholz_CourseProject_Part2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.PrintButton = new System.Windows.Forms.Button();
+            this.EmployeesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.AddButton.Location = new System.Drawing.Point(12, 12);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(115, 30);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.BackColor = System.Drawing.Color.LightCoral;
+            this.RemoveButton.Location = new System.Drawing.Point(133, 12);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(115, 30);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = false;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.BackColor = System.Drawing.Color.Silver;
+            this.DisplayButton.Location = new System.Drawing.Point(254, 12);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(115, 30);
+            this.DisplayButton.TabIndex = 2;
+            this.DisplayButton.Text = "Display";
+            this.DisplayButton.UseVisualStyleBackColor = false;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.BackColor = System.Drawing.Color.Silver;
+            this.PrintButton.Location = new System.Drawing.Point(375, 12);
+            this.PrintButton.Name = "PrintButton";
+            this.PrintButton.Size = new System.Drawing.Size(115, 30);
+            this.PrintButton.TabIndex = 3;
+            this.PrintButton.Text = "Print Paychecks";
+            this.PrintButton.UseVisualStyleBackColor = false;
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // EmployeesListBox
+            // 
+            this.EmployeesListBox.BackColor = System.Drawing.Color.LightGray;
+            this.EmployeesListBox.FormattingEnabled = true;
+            this.EmployeesListBox.Location = new System.Drawing.Point(12, 57);
+            this.EmployeesListBox.Name = "EmployeesListBox";
+            this.EmployeesListBox.Size = new System.Drawing.Size(478, 186);
+            this.EmployeesListBox.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 255);
+            this.Controls.Add(this.EmployeesListBox);
+            this.Controls.Add(this.PrintButton);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Name = "MainForm";
+            this.Text = "Payroll System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Button PrintButton;
+        private System.Windows.Forms.ListBox EmployeesListBox;
+    }
+}
+
---
+++ Buchholz_CourseProject_Part2/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buchholz_CourseProject_Part2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            //Add Employee to EmployeeListBox
+            EmployeesListBox.Items.Add("New Employee");
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            //Remove Selected Employee from EmployeeListBox
+            int itemNumber = EmployeesListBox.SelectedIndex;
+
+            if(itemNumber > -1)
+            {
+                EmployeesListBox.Items.RemoveAt(itemNumber);
+            }
+            else
+            {
+                MessageBox.Show("Please Select an Employee to Remove.");
+            }
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Displaying All Employees...");
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Printing All Employee Paychecks...");
+        }
+    }
+}
